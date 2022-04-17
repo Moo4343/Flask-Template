905ed6873920759a2dfbcc30aa0c865bafa64307
Moo4343 <vieraissam@gmail.com>
2022-04-17 18:10:12 +0200

    changed things

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from requests import get
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+
+
+
+
 @app.route("/", methods=["GET"])
 def home():
+
     return render_template("index.html", ip = get('https://api.ipify.org').text)
 
 if __name__ == "__main__":
