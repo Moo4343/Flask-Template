d82ab14a00f915822e45f3788bd1bdd77dad0fe3
Moo4343 <vieraissam@gmail.com>
2022-04-17 18:06:26 +0200

    modified things

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from requests import get
-from crypt import methods
 from flask import Flask, render_template, request, jsonify
-from crypt import *
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    ip = get('https://api.ipify.org').text
     return render_template("index.html", ip = get('https://api.ipify.org').text)
 
 if __name__ == "__main__":
